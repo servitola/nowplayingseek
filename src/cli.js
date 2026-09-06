09f0ff528f4556835b3df0a81baa37ce91a1091b
servitola <servitola@gmail.com>
2026-09-07 01:10:00 +0300

    nowplayingseek 2026.09.07

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 ObjC.import('stdlib');
 
-const VERSION = '2026.09.05';
+const VERSION = '2026.09.07';
 const PROGRESSIVE_FLAG = '--progressive';
 const HOLD_FLAG = '--hold';
 const KNOB_FLAG = '--knob';
